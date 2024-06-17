6e2ad53c6f83daeeb421b195bc3c32960f10b813
anas <anasmj7@gmail.com>
2024-06-17 17:04:11 +0600

    m

--- lib/src/app.body.dart
+++ lib/src/app.body.dart
@@ -1,3 +1,6 @@
+import 'dart:typed_data';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class CustomPaintExample extends StatefulWidget {
@@ -19,7 +22,7 @@ class _CustomPaintExampleState extends State<CustomPaintExample> {
           color: Colors.grey.shade200,
           child: CustomPaint(
             size: const Size(300, 300),
-            painter: RectPainter(),
+            painter: PointPainter(),
           ),
         ),
       ),
@@ -27,39 +30,33 @@ class _CustomPaintExampleState extends State<CustomPaintExample> {
   }
 }
 
-class RectPainter extends CustomPainter {
-  final rectPainter = Paint()
+class PointPainter extends CustomPainter {
+  final painter = Paint()
     ..color = Colors.black
-    ..style = PaintingStyle.stroke;
+    ..strokeWidth = 5
+    ..strokeCap = StrokeCap.round;
   @override
   void paint(Canvas canvas, Size size) {
-    final center = Offset(size.width / 2, size.height / 2);
-    //Center offset + with
-    final rect1 = Rect.fromCenter(
-      center: center,
-      width: size.width * 0.5,
-      height: size.height * 0.5,
+    final points = <Offset>[
+      Offset.zero,
+      Offset(size.width, 0),
+      Offset(size.width / 2, size.height / 2),
+      Offset.zero,
+    ];
+    final rawPoints = Float32List.fromList(
+      [
+        0,
+        0,
+        size.width,
+        0,
+        size.width / 2,
+        size.height / 2,
+        0,
+        0,
+      ],
     );
-
-    ///Rect from values of a circle
-    final rect2 = Rect.fromCircle(center: center, radius: 40);
-
-    // Renders difference between left ||right and top ||bottom
-    const rect3 = Rect.fromLTRB(20, 20, 100, 100);
-
-    //roudned corners
-    canvas.drawRRect(
-      RRect.fromRectAndRadius(rect3, const Radius.circular(10)),
-      rectPainter,
-    );
-
-    // roudned specific corners
-    canvas.drawRRect(
-      RRect.fromRectAndCorners(rect2, topLeft: const Radius.circular(10)),
-      rectPainter,
-    );
-    //different radius in x and y axis
-    canvas.drawRRect(RRect.fromRectXY(rect1, 20, 5), rectPainter);
+    // canvas.drawPoints(PointMode.polygon, points, painter);
+    canvas.drawRawPoints(PointMode.polygon, rawPoints, painter);
   }
 
   @override
